99f822318601caa718439612ba2c2defd24b9baf
michael <mymis168@gmaile.com>
2026-03-18 14:05:25 +0800

    schedule 2

--- schedule_demo.py
+++ schedule_demo.py
@@ -6,13 +6,27 @@ pip install schedule
 """
 import schedule
 import time
+from datetime import datetime
 
 def run_every_5seconds():
-    print("每 5秒執行一次")
+    print(f"每 5秒執行一次")
 
+def run_every_60seconds():
+    print(f"每 60秒執行一次")
+
+def run_at_spec_time():
+    now= datetime.now()
+    print(f"觸發時間: {now}")
 
 #只是安排一個任務 
 schedule.every(5).seconds.do(run_every_5seconds)
+schedule.every(1).minutes.do(run_every_60seconds)
+schedule.every().day.at("14:06").do(run_at_spec_time)
+# schedule.every().hours.at(":31").do()
+# schedule.every().friday.at("16:30").do()
+
+
+
 
 while True:
     #要求 schedule 每n單位 檢查一次是否有任務需要執行
