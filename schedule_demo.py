5a6a677462a25645aec2135e8c9ba933973a2d01
michael <mymis168@gmaile.com>
2026-03-18 14:27:28 +0800

    end 更新

--- schedule_demo.py
+++ schedule_demo.py
@@ -18,6 +18,16 @@ def run_at_spec_time():
     now= datetime.now()
     print(f"觸發時間: {now}")
 
+def run_and_cancelJob():
+    # 本任務視情況 決定是否繼續執行, 例如各分點的檔案在營業結束後要上傳 pos csv file
+    # schedule 沒有提供設定開始的邏輯 , 必須在 func內自己判斷時間是否滿足開始執行
+    cnt = True
+    if( cnt ):
+        return schedule.cancel_job   #通知 schedule 不用再安排後續的任務了
+
+
+
+
 #只是安排一個任務 
 schedule.every(5).seconds.do(run_every_5seconds)
 schedule.every(1).minutes.do(run_every_60seconds)
@@ -25,7 +35,9 @@ schedule.every().day.at("14:06").do(run_at_spec_time)
 # schedule.every().hours.at(":31").do()
 # schedule.every().friday.at("16:30").do()
 
-
+# 設定結束或取消的方式
+schedule.every(30).seconds.do(run_every_5seconds).until("13:30")  # 開始每30秒執行任務直到 13:30 結束
+schedule.every().hours.at(":50").do(run_and_cancelJob)   # 一旦檔案下載完成 自動結束
 
 
 while True:
